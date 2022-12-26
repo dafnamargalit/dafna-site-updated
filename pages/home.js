7eaea8128116491bd477ebfd3dabc5f70ae7057c
Dafna <margalit.dafna77@gmail.com>
2022-12-25 16:47:23 -0800

    click to enter (Allows time for rendering)

---
+++ pages/home.js
@@ -0,0 +1,120 @@
+import Head from 'next/head';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Link from 'next/link';
+import Image from 'next/image';
+import D from '/letters/D.svg';
+import AP from '/letters/A-P.svg';
+import F from '/letters/F.svg';
+import N from '/letters/N.svg';
+import AY from '/letters/A-Y.svg';
+import Modal from 'components/Modal';
+import StarrySkies from 'components/Three/StarrySkies';
+import ProjectIcon from 'components/ProjectIcon';
+import VideoGame from 'images/ily.png';
+
+export default function Home() {
+
+  const [modal, setModal] = useState(true);
+  
+  	useEffect(() => {
+    if(localStorage.getItem('seen') === 'true'){
+      console.log('not seen hi', localStorage.getItem('seen'));
+      localStorage.setItem('seen', 'true');
+      setModal(false);
+    }
+    });
+
+    const handleClose = () => {
+      setModal(false);
+    };
+
+  return (
+      <Container>
+        <Head>
+        <title>Dafna</title>
+        <meta name="description" content="Welcome to Dafna's Website" />
+         <link rel="shortcut icon" href="/D-square.png" />
+      </Head>
+        {console.log(StarrySkies)}
+        <StarrySkies />
+      <Content>
+        <VideoGameButtonWrap>
+          <Link href="/games">
+            <ProjectIcon src={VideoGame} />
+          </Link>
+        </VideoGameButtonWrap>
+        <Modal show={modal} onClose={() => handleClose()} />
+        <WrapLogo>
+          <Link href="/dates" style={{ textDecoration: 'none' }}>
+							<Letters src={D} alt="D" />
+						</Link>
+						<Link href="/apparel" style={{ textDecoration: 'none' }}>
+							<Letters src={AY} alt="A" />
+						</Link>
+						<Link href="/footage" style={{ textDecoration: 'none' }}>
+							<Letters src={F} alt="F" />
+						</Link>
+						<Link href="/news" style={{ textDecoration: 'none' }}>
+							<Letters src={N} alt="N" />
+						</Link>
+						<Link href="/about" style={{ textDecoration: 'none' }}>
+							<Letters src={AP} alt="A" />
+						</Link>
+        </WrapLogo>
+      </Content>
+      </Container>
+  )
+}
+
+const Container = styled.div`
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const VideoGameButtonWrap = styled.div`
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  z-index: 9999;
+`;
+
+const WrapLogo = styled.div`
+  display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const Letters = styled(Image)`
+	will-change: transform;
+	z-index: 1000;
+	width: 17vh;
+	padding: 0.5em;
+	transition: transform .2s;
+	&:hover{
+		transform: scale(1.2);
+	}
+
+	@media (max-width: 1024px) { 
+		width: 6vh;
+		padding: .2em;
+	}
+`;
+
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,12 @@
 import Head from 'next/head';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 import Image from 'next/image';
-import D from '/letters/D.svg';
-import AP from '/letters/A-P.svg';
-import F from '/letters/F.svg';
-import N from '/letters/N.svg';
-import AY from '/letters/A-Y.svg';
-import Modal from 'components/Modal';
-import StarrySkies from 'components/Three/StarrySkies';
-import ProjectIcon from 'components/ProjectIcon';
-import VideoGame from 'images/ily.png';
 
-export default function Home() {
+export default function Index() {
 
-  const [modal, setModal] = useState(true);
-  
-  	useEffect(() => {
-    if(localStorage.getItem('seen') === 'true'){
-      console.log('not seen hi', localStorage.getItem('seen'));
-      localStorage.setItem('seen', 'true');
-      setModal(false);
-    }
-    });
-
-    const handleClose = () => {
-      setModal(false);
-    };
+  const [clicked, setClicked] = useState(false);
 
   return (
       <Container>
@@ -36,32 +15,11 @@ export default function Home() {
         <meta name="description" content="Welcome to Dafna's Website" />
          <link rel="shortcut icon" href="/D-square.png" />
       </Head>
-        <StarrySkies />
-      <Content>
-        <VideoGameButtonWrap>
-          <Link href="/games">
-            <ProjectIcon src={VideoGame} />
-          </Link>
-        </VideoGameButtonWrap>
-        <Modal show={modal} onClose={() => handleClose()} />
-        <WrapLogo>
-          <Link href="/dates" style={{ textDecoration: 'none' }}>
-							<Letters src={D} alt="D" />
-						</Link>
-						<Link href="/apparel" style={{ textDecoration: 'none' }}>
-							<Letters src={AY} alt="A" />
-						</Link>
-						<Link href="/footage" style={{ textDecoration: 'none' }}>
-							<Letters src={F} alt="F" />
-						</Link>
-						<Link href="/news" style={{ textDecoration: 'none' }}>
-							<Letters src={N} alt="N" />
-						</Link>
-						<Link href="/about" style={{ textDecoration: 'none' }}>
-							<Letters src={AP} alt="A" />
-						</Link>
-        </WrapLogo>
-      </Content>
+      <Link href="/home">
+       {clicked ? <Text>ENTERING...</Text> : <WelcomeButton onClick={() => setClicked(true)}>
+         CLICK HERE TO ENTER DAFNA'S WEBSITE
+        </WelcomeButton>}
+      </Link>
       </Container>
   )
 }
@@ -71,49 +29,26 @@ const Container = styled.div`
   padding: 0;
   height: 100vh;
   width: 100vw;
-`;
-
-const VideoGameButtonWrap = styled.div`
-  position: absolute;
-  height: 100vh;
-  width: 100vw;
   display: flex;
-  justify-content: flex-end;
-`;
-
-
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  color: white;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  position: absolute;
-  z-index: 9999;
+  background: #000000;
 `;
 
-const WrapLogo = styled.div`
-  display: flex;
-	align-items: center;
-	justify-content: center;
+const WelcomeButton = styled.button`
+  background: #000000;
+  border: 1px solid #ffffff;
+  color: #ffffff;
+  padding: 10px 20px;
+  font-family: "Courier New", Courier, monospace;
+  &:hover {
+    cursor: pointer;
+    background: #1e1f1f;
+  }
 `;
 
-const Letters = styled(Image)`
-	will-change: transform;
-	z-index: 1000;
-	width: 17vh;
-	padding: 0.5em;
-	transition: transform .2s;
-	&:hover{
-		transform: scale(1.2);
-	}
-
-	@media (max-width: 1024px) { 
-		width: 6vh;
-		padding: .2em;
-	}
+const Text = styled.div`
+  color: #ffffff;
+  text-decoration: none;
+   font-family: "Courier New", Courier, monospace;
 `;
-
