6752a4092807d849a84b826c0c81c3e461cb69ea
Dafna <margalit.dafna77@gmail.com>
2022-12-25 20:51:37 -0800

    merch page

---
+++ pages/apparel.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import Navbar from '/components/Navbar';
+import styled from 'styled-components';
+import merch from '/images/merch.png';
+import A from '/letters/A-Y.svg';
+import P from '/letters/P-Y.svg';
+import R from '/letters/R-Y.svg';
+import E from '/letters/E-Y.svg';
+import L from '/letters/L-Y.svg';
+import StarrySkies from 'components/Three/StarrySkies';
+import Image from 'next/image';
+import { IconLeftArrow } from 'icons';
+import { theme } from 'theme';
+import Link from 'next/link';
+const Apparel = () => {
+    return(
+        <ApparelWrap>
+            <StarrySkies />
+        <NavbarHeader>
+            <Navbar width={"5vh"} padding={"0.2em"} />
+        </NavbarHeader>
+                     <LeftArrowIconWrap>
+            <Link href="/home">
+                <LeftArrowIcon />
+            </Link>
+        </LeftArrowIconWrap>
+        <Content>
+        <Header>
+            <Title>
+                <Letter src={A}></Letter>
+                <Letter src={P}></Letter>
+                <Letter src={P}></Letter>
+                <Letter src={A}></Letter>
+                <Letter src={R}></Letter>
+                <Letter src={E}></Letter>
+                <Letter src={L}></Letter>
+            </Title>
+        </Header>
+        <Body>
+
+           <MerchImage src={merch} alt=""/>
+
+           <TextLink href="https://shop.dafna.rocks">
+                VISIT MERCH STORE  <span style={{fontFamily: "sans-serif"}}>&#8594;</span>
+            </TextLink>
+        </Body>
+        </Content>
+        </ApparelWrap>
+    )
+}
+
+const ApparelWrap = styled.div`
+    height: 100vh;
+    width: 100vw;
+`
+const NavbarHeader = styled.div`
+    width: 100vw;
+    position: absolute;
+    top: -50px;
+    z-index: 10001;
+`
+const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 75px;
+    transition: all 0.5s ease 0s;
+    // border-bottom: 1px solid #b9c0ff;
+    user-select: none;
+`
+
+const Letter = styled(Image)`
+    width: 7vh;
+    padding: 0.25vh;
+    @media (max-width: 400px){
+        width: 5vh;
+    }
+`
+
+
+const Title = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+    color: #ffdd00;
+    text-align: center;
+    font-size: 24px;
+    width: 100%;
+    justify-content: center;
+    height: 50px;
+    @media (max-width: 675px) {
+    font-size: 20px;
+    }
+`
+const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100vw;
+    align-items: center;
+    color: white;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+    z-index: 9999;
+`;
+
+const Body = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content:center;
+
+`
+const MerchImage = styled(Image)`
+    width: 40vh;
+    height: auto;
+    @media (max-width: 800px){
+        width: 35vh;
+    }
+`
+
+const TextLink = styled.a`
+    color:#ffdd00;
+    font-weight: 800;
+    text-shadow:  0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+    text-decoration: none;
+    &:hover{
+        transform: scale(1.1);
+    }
+`
+
+const LeftArrowIconWrap = styled.div`
+  width: 50px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 1em;
+  z-index: 10002;
+`;
+
+const LeftArrowIcon = styled(IconLeftArrow)`
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    &:hover {
+        stroke: ${theme.COLOR.HOT_PINK};
+    }
+`;
+
+export default Apparel;
